1815430c3fa5e2248619da0cd32fed029655caf5
Ismail099 <hedaribo09@gmail.com>
2022-12-25 14:56:28 +0100

    Tested!

--- script.js
+++ script.js
@@ -9,9 +9,16 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const currentTime = new Date()
     let yearOfTheEvent = currentTime.getFullYear()
-    let eventDate = new Date(yearOfTheEvent, 00, 27)
-    const isItJanuary1st = currentTime.getMonth === 01 && currentTime.getDate() === 27
+    let eventDate = new Date(yearOfTheEvent, 11, 25, 14, 50, 00, 00)
+    const isItJanuary1st = currentTime.getMonth() === 11 && currentTime.getDate() === 25 && 
+                           currentTime.getHours() === 14 && currentTime.getMinutes() === 50
 
+    console.log(currentTime.getMonth() + "." + currentTime.getDate() + " " + 
+                currentTime.getMinutes() + ":" + currentTime.getSeconds())
+
+    // let eventDate = new Date(yearOfTheEvent, 00, 27)
+    // const isItJanuary1st = currentTime.getMonth === 00 && currentTime.getDate() === 27
+    
 
     /**
      * @brief countdown Function which counts down to the specified date.
@@ -20,10 +27,13 @@ document.addEventListener('DOMContentLoaded', function(){
         const now = new Date()
 
         if (now > eventDate) {
-            eventDate = new Date(yearOfTheEvent + 1, 00, 27)
+            // eventDate = new Date(yearOfTheEvent + 1, 00, 27)
+            eventDate = new Date(yearOfTheEvent + 1, 11, 25)
         }
-        else if (now.getFullYear(), 00, 27) {
-            eventDate = new Date(now.getFullYear(), 00, 27)
+        else if (now.getFullYear() === eventDate.getFullYear() + 1) {
+            // eventDate = new Date(now.getFullYear(), 00, 27)
+            eventDate = new Date(now.getFullYear(), 11, 25)
+            // eventDate = new Date("2022-12-25")
         }
 
         const currentTime = now.getTime()
@@ -33,13 +43,14 @@ document.addEventListener('DOMContentLoaded', function(){
         let seconds = Math.floor(remainingTime / 1000)
         let minutes = Math.floor(seconds / 60)
         let hours = Math.floor(minutes / 60)
-        let days = Math.floor(hours / 24)
+        let days = Math.floor(hours / 24)        
 
         hours %= 24
         minutes %= 60
         seconds %= 60
 
         if (isItJanuary1st) {
+            alert('Happ Birthday, Bahar 🥳🥳')
             console.log('Happ Birthday, Bahar 🥳🥳')
             
             countdownContainer.style.display = "none"
@@ -54,4 +65,5 @@ document.addEventListener('DOMContentLoaded', function(){
             setTimeout(countdown, 1000)
         } 
     } // End of countdown
+    countdown()
 }) // end of DOMContentLoaded
